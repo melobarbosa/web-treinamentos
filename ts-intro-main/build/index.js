38704179dc41763323b2d48d6ee27d2dd142f50a
BiankaOl <146958610+BiankaOl@users.noreply.github.com>
2023-11-13 17:19:53 -0300

    atualizando3

---
+++ ts-intro-main/build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+var a = 10;
+var b = 5;
+var c = a + b;
+console.log(`a soma de ${a} + ${b}  é = ${c}`);
---
+++ ts-intro-main/index.ts
@@ -0,0 +1,5 @@
+var a: number = 10
+var b: number = 5
+var c  = a + b
+
+console.log(`a soma de ${a} + ${b}  é = ${c}`)
